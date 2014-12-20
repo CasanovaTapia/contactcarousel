b8807077b808eea475d631213e8aa31b784364d6
CasanovaTapia <ching@casanovatapia.com>
2014-12-19 16:20:55 -0800

    Authorization with Pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::Base
+  include Pundit
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  rescue_from Pundit::NotAuthorizedError do |exception|
+    redirect_to new_transaction_path, alert: "You must have a subscription to access this page."
+  end
+
   protected
-  
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation)}
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :password_confirmation)}
